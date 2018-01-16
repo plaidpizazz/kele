7fca6f12350497436f889eb0a350cceb96a53fec
Otexas <marcellus.spears@gmail.com>
2018-01-16 15:41:38 -0600

    kele api roadmaps and checkpoints: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,9 +1,11 @@
 require 'httparty'
 require 'json'
+require './lib/roadmap'
 
 class Kele
 
   include HTTParty
+  include Roadmap
 
   base_uri 'https://www.bloc.io/api/v1'
 
@@ -17,13 +19,13 @@ class Kele
   end
 
   def get_me
-    response = self.class.get('/users/me', headers: { "authorization" => @user_auth_token })
+    response = self.class.get("/users/me", headers: { "authorization" => @user_auth_token })
     JSON.parse(response.body)
   end
 
   def get_mentor_availability
-    # @mentor_id = 2292457
-    response = self.class.get('/mentors/2292457/student_availability', headers: { "authorization" => @user_auth_token })
+    # example mentor_id = 2292457
+    response = self.class.get("/mentors/2292457/student_availability", headers: { "authorization" => @user_auth_token })
     JSON.parse(response.body)
   end
 
